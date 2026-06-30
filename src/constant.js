85d09e71d802bb8a1472298154b08f5b4d45ebc0
Vishal Rajput <vr5202978@gmail.com>
2026-06-30 16:22:18 +0530

    fixed errors

--- src/constant.js
+++ src/constant.js
@@ -1,1 +1,2 @@
 export const db_Name = "Backend"
+
--- src/db/index.js
+++ src/db/index.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-import { db_Name } from "../constant.js";
+import  {db_Name}  from "../constant.js";
 
  const connectDb = async () =>{
     try{
-            const dbInstance = await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+            const dbInstance = await  mongoose.connect(`${process.env.MONGODB_URI}/${db_Name}`);
             console.log(`database connection successfully ${dbInstance.Connection.host}`);
 
     } catch(err){
